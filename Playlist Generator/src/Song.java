b40c1b6b54c53702ccbaa9b3268fbbf966b94145
Roza <rza.shv@gmail.com>
2026-06-04 18:48:03 +0900

    implement Song model class

--- Playlist Generator/src/Song.java
+++ Playlist Generator/src/Song.java
@@ -1,5 +1,4 @@
 import java.util.List;
-import java.util.ArrayList;
 
 
 public class Song {
@@ -10,11 +9,45 @@ public class Song {
 	private String filePath;
 	private List<String> tags;
 	
+	public Song(String title, String artist, 
+			String genre, String album,
+			String filePath, List<String> tags) {
+		this.title = title;
+		this.artist = artist;
+		this.genre = genre;
+		this.album = album;
+		this.filePath = filePath;
+		this.tags = tags;
+	}
+	
 	public String getInfo() {
-		return " ";
+		return "Title: " + title + 
+				"\nArtist: " + artist + 
+				"\nGenre: " + genre + 
+				"\nAlbum: " + album;
 	}
 	
 	public String getTitle() {
-		return " ";
+		return title;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public List<String> getTags() {
+		return tags;
+	}
+	
+	@Override
+	public String toString() {
+		return title + " - " + artist;
 	}
 }
